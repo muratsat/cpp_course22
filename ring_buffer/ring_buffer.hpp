aa1e715b98b5a42196f7906caa03c34bee0704a1
muratsat <19-muratsat@users.noreply.gitlab.com>
2022-10-19 20:13:28 +0300

    ring_buffer solution

--- ring_buffer/ring_buffer.hpp
+++ ring_buffer/ring_buffer.hpp
@@ -3,23 +3,38 @@
 #include <cstddef>
 
 class RingBuffer {
+  int* item_;
+  int capacity_;
+  int size_ = 0;
+  int begin_ = 0;
+  int end_ = 0;
+
  public:
-  explicit RingBuffer(size_t capacity) {
+  explicit RingBuffer(size_t capacity) : capacity_(capacity) {
+    item_ = new int[capacity];
   }
 
-  size_t Size() const {
-    return 0;
-  }
+  size_t Size() const { return size_; }
 
-  bool Empty() const {
-    return false;
-  }
+  bool Empty() const { return size_ == 0; }
 
   bool TryPush(int element) {
+    int end = (end_ + 1) % capacity_;
+    if (end == begin_) {
+      return false;
+    }
+    item_[end] = element;
+    size_++;
     return true;
   }
 
   bool TryPop(int* element) {
+    if (size_ == 0) {
+      return false;
+    }
+    *element = item_[begin_];
+    begin_ = (begin_ + 1) % capacity_;
+    size_--;
     return true;
   }
 };
