c618c7a8d1b06fa6e368414080ebe004d22e0433
muratsat <muratsat@users.noreply.github.com>
2022-10-19 10:45:11 +0300

    factorization handle prime argument

--- factorization/factorization.hpp
+++ factorization/factorization.hpp
@@ -4,13 +4,15 @@
 std::vector<int> Factorize(int n) {
   std::vector<int> factors;
 
-  for (int i = 2; i <= n; i++) {
-    if (n % i == 0) {
-      while (n % i == 0) {
-        factors.push_back(i);
-        n /= i;
-      }
+  for (int i = 2; i * i <= n; i++) {
+    while (n % i == 0) {
+      factors.push_back(i);
+      n /= i;
     }
+  }
+
+  if (n != 1) {
+    factors.push_back(n);
   }
 
   return factors;
