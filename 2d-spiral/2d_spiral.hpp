0ffad5a1a985d6e2c4896d9427823a16f78788ca
muratsat <muratsat@users.noreply.github.com>
2022-10-19 11:16:15 +0300

    2d spiral solution

--- 2d-spiral/2d_spiral.hpp
+++ 2d-spiral/2d_spiral.hpp
@@ -1,3 +1,33 @@
 int** MakeSpiral(int n) {
+  int** grid = new int*[n];
+  for (int i = 0; i < n; i++) {
+    grid[i] = new int[n];
+  }
 
+  //          R   D  L  U
+  int dx[] = {0, 1, 0, -1};
+  int dy[] = {1, 0, -1, 0};
+
+  int x = 0, y = 0;
+  int direction = 0;
+
+  for (int i = 1; i <= n * n; i++) {
+    grid[x][y] = i;
+    int tx = x + dx[direction];
+    int ty = y + dy[direction];
+
+    bool x_is_out = tx < 0 || tx >= n;
+    bool y_is_out = ty < 0 || ty >= n;
+
+    if (x_is_out || y_is_out || grid[tx][ty] != 0) {
+      direction = (direction + 1) % 4;
+      tx = x + dx[direction];
+      ty = y + dy[direction];
+    }
+
+    x = tx;
+    y = ty;
+  }
+
+  return grid;
 }
