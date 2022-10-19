a30eb69495f9e6c51d29b0a2c13436c676e6db55
muratsat <19-muratsat@users.noreply.gitlab.com>
2022-10-19 18:46:20 +0300

    bin_search: removed auto type

--- bin_search/bin_search.hpp
+++ bin_search/bin_search.hpp
@@ -2,7 +2,7 @@
 
 int* LowerBound(int* first, int* last, int value) {
   while (last > first + 1) {
-    auto mid = first + (last - first) / 2;
+    int* mid = first + (last - first) / 2;
 
     if (*mid < value) {
       first = mid;
