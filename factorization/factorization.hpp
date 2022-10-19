b1caa1fb216ac8739e72af4fb3ffe0dde81b62b8
muratsat <muratsat@users.noreply.github.com>
2022-10-19 10:29:51 +0300

    factorization solution

--- factorization/factorization.hpp
+++ factorization/factorization.hpp
@@ -1,7 +1,17 @@
 #pragma once
 #include <vector>
 
-std::vector<int> Factorize(int /*n*/) {
-    /* Not Implemented */
-    return {};
+std::vector<int> Factorize(int n) {
+  std::vector<int> factors;
+
+  for (int i = 2; i <= n; i++) {
+    if (n % i == 0) {
+      factors.push_back(i);
+      while (n % i == 0) {
+        n /= i;
+      }
+    }
+  }
+
+  return factors;
 }
