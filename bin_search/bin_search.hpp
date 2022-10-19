575964c891a3bea917ab0f71d278c3013d723289
muratsat <19-muratsat@users.noreply.gitlab.com>
2022-10-19 18:44:41 +0300

    bin_search solution

--- bin_search/bin_search.hpp
+++ bin_search/bin_search.hpp
@@ -1,6 +1,15 @@
 #pragma once
 
-int* LowerBound(int* /* first */, int* /* last */, int /* value */) {
-    /* Not Implemented */
-    return nullptr;
+int* LowerBound(int* first, int* last, int value) {
+  while (last > first + 1) {
+    auto mid = first + (last - first) / 2;
+
+    if (*mid < value) {
+      first = mid;
+    } else {
+      last = mid;
+    }
+  }
+
+  return last;
 }
