6a5651929505deb90e76651421a707e91a50fd41
muratsat <19-muratsat@users.noreply.gitlab.com>
2022-10-22 11:07:40 +0300

    2d_spiral initialize grid with zeros

--- 2d-spiral/2d_spiral.hpp
+++ 2d-spiral/2d_spiral.hpp
@@ -1,25 +1,24 @@
 int** MakeSpiral(int n) {
   int** grid = new int*[n];
-  for (int i = 0; i < n; i++) {
-    grid[i] = new int[n];
+  for (int i = 0; i < n; ++i) {
+    grid[i] = new int[n]();
   }
-  //          R   D  L  U
   int dx[] = {0, 1, 0, -1};
   int dy[] = {1, 0, -1, 0};
   int x = 0;
   int y = 0;
-  int direction = 0;
-  const int kDirectoinsSize = 4;
+  int dir = 0;
+  const int kDirections = 4;
   for (int i = 1; i <= n * n; i++) {
     grid[x][y] = i;
-    int tx = x + dx[direction];
-    int ty = y + dy[direction];
-    bool x_is_out = tx < 0 || tx >= n;
-    bool y_is_out = ty < 0 || ty >= n;
-    if (x_is_out || y_is_out || grid[tx][ty] != 0) {
-      direction = (direction + 1) % kDirectoinsSize;
-      tx = x + dx[direction];
-      ty = y + dy[direction];
+    int tx = x + dx[dir];
+    int ty = y + dy[dir];
+    bool is_tx_out = tx < 0 || tx >= n;
+    bool is_ty_out = ty < 0 || ty >= n;
+    if (is_tx_out || is_ty_out || grid[tx][ty] != 0) {
+      dir = (dir + 1) % kDirections;
+      tx = x + dx[dir];
+      ty = y + dy[dir];
     }
     x = tx;
     y = ty;
