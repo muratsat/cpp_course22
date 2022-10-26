76e35ce8e03d6079616122ba68a2f7a6494f52e0
muratsat <satybaldiev.m@phystech.edu>
2022-10-26 19:31:14 +0300

    big int: constructors

--- big_integer/big_integer.cpp
+++ big_integer/big_integer.cpp
@@ -1,1 +1,101 @@
 #include "big_integer.hpp"
+
+#include <iostream>
+
+static int Max(int a, int b) { return a > b ? a : b; }
+
+BigInt::BigInt(int64_t n) {
+  if (n == 0) {
+    return;
+  }
+  digits.push_back(n % base_);
+  if (n / base_) {
+    digits.push_back(n / base_);
+  }
+}
+
+BigInt& BigInt::operator=(const BigInt& other) {
+  digits = other.digits;
+  is_negative_ = other.is_negative_;
+  return *this;
+}
+
+BigInt::BigInt(std::string s) {
+  BigInt dec(1);
+
+  for (int i = s.size() - 1; i >= 0; i--) {
+    if (s[i] == '-') {
+      is_negative_ = true;
+      break;
+    }
+    BigInt to_add = dec * (s[i] - '0');
+    AddAbs(to_add);
+    dec *= 10;
+  }
+}
+
+void BigInt::AddAbs(const BigInt& to_add) {
+  size_t old_size = digits.size();
+  size_t max_size = Max(old_size, to_add.digits.size());
+  digits.resize(max_size);
+
+  int64_t carry = 0;
+  for (size_t i = 0; i < max_size; i++) {
+    int64_t tmp = carry;
+    if (i < old_size) {
+      tmp += digits[i];
+    }
+    if (i < to_add.digits.size()) {
+      tmp += to_add.digits[i];
+    }
+
+    digits[i] = tmp % base_;
+    carry = tmp / base_;
+  }
+
+  if (carry) {
+    digits.push_back(carry);
+  }
+
+  Normalize();
+}
+
+void BigInt::Normalize() {
+  size_t new_size = digits.size();
+  while (new_size > 0 && digits[new_size - 1] == 0) {
+    new_size--;
+  }
+  digits.resize(new_size);
+}
+
+BigInt& BigInt::operator*=(int x) {
+  if (x < 0) {
+    is_negative_ = !is_negative_;
+    x = -x;
+  }
+
+  int64_t carry = 0;
+  for (int i = 0; i < digits.size(); i++) {
+    int64_t tmp = digits[i] * x + carry;
+    digits[i] = tmp % base_;
+    carry = tmp / base_;
+  }
+  if (carry) {
+    digits.push_back(carry);
+  }
+
+  Normalize();
+  return *this;
+}
+
+BigInt operator*(const BigInt big_int, const long long x) {
+  BigInt res(big_int);
+  res *= x;
+  return res;
+}
+
+BigInt operator*(const long long x, const BigInt big_int) {
+  BigInt res(big_int);
+  res *= x;
+  return res;
+}
--- big_integer/big_integer.hpp
+++ big_integer/big_integer.hpp
@@ -1,6 +1,34 @@
 #pragma once
+#include <cstdint>
+#include <iostream>
+#include <string>
 #include <vector>
 
 class BigInt {
-    /* Not Implemented */
+ public:
+  BigInt() = default;
+  BigInt(int64_t n);
+  BigInt(std::string s);
+  BigInt(const BigInt& other) { *this = other; }
+  ~BigInt(){};
+
+  BigInt& operator=(const BigInt& other);
+
+  BigInt& operator*=(int x);
+  friend BigInt operator*(const BigInt big_int, const long long x);
+  friend BigInt operator*(const long long x, const BigInt big_int);
+
+  size_t Size() const { return digits.size(); }
+
+ private:
+  const long long unsigned base_ = 4294967296;
+  // const long long unsigned base_ = 10;
+  std::vector<unsigned> digits;
+  bool is_negative_ = false;
+
+  // add absolute values
+  void AddAbs(const BigInt& to_add);
+
+  // remove trailling zeros
+  void Normalize();
 };
