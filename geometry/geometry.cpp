0893b5ebf532ab0388c509e4d36d7a90bc3a1b6c
muratsat <satybaldiev.m@phystech.edu>
2022-12-02 13:06:16 +0300

    refactor geometry.cpp

--- geometry/geometry.cpp
+++ geometry/geometry.cpp
@@ -4,7 +4,9 @@
 
 Point::Point() {}
 Point::Point(const Vector& vector) { base_ = vector; }
-Point::Point(int64_t x, int64_t y) { base_ = Vector(x, y); }
+Point::Point(int64_t x_coord, int64_t y_coord) {
+  base_ = Vector(x_coord, y_coord);
+}
 Point::~Point() {}
 
 bool Point::ContainsPoint(const Point& point) const {
@@ -12,10 +14,9 @@ bool Point::ContainsPoint(const Point& point) const {
 }
 
 bool Point::CrossesSegment(const Segment& segment) const {
-  const Point& p = *this;
-  Vector va = segment.GetA() - p;
-  Vector vb = segment.GetB() - p;
-  return (va ^ vb) == 0 && (va * vb) <= 0;
+  Vector vector_a = segment.GetA() - *this;
+  Vector vector_b = segment.GetB() - *this;
+  return (vector_a ^ vector_b) == 0 && (vector_a * vector_b) <= 0;
 }
 
 Point* Point::Clone() const { return new Point(base_); }
@@ -39,10 +40,10 @@ bool Segment::ContainsPoint(const Point& point) const {
 }
 
 bool Segment::CrossesSegment(const Segment& segment) const {
-  const Vector& d1 = direction_vector_;
-  const Vector& d2 = segment.direction_vector_;
+  const Vector& dir1 = direction_vector_;
+  const Vector& dir2 = segment.direction_vector_;
 
-  if ((d1 ^ d2) == 0) {
+  if ((dir1 ^ dir2) == 0) {
     // segments are collinear;
     bool cross = ContainsPoint(segment.GetA()) ||
                  ContainsPoint(segment.GetB()) ||
@@ -50,16 +51,16 @@ bool Segment::CrossesSegment(const Segment& segment) const {
     return cross;
   }
 
-  const Vector& b1 = base_;
-  const Vector& b2 = segment.base_;
-  // b1 + d1 * s1 = b2 + d2 * s2;
+  const Vector& base1 = base_;
+  const Vector& base2 = segment.base_;
+  // base1 + dir1 * scale1 = base2 + dir2 * scale2;
   // if segments intersect,
-  // then 0 <= s1 <= 1 and 0 <= s2 <= 1
+  // then 0 <= scale1 <= 1 and 0 <= scale2 <= 1
 
-  double s1 = (double)((b2 - b1) ^ d2) / (double)(d1 ^ d2);
-  double s2 = (double)((b1 - b2) ^ d1) / (double)(d2 ^ d1);
+  double scale1 = (double)((base2 - base1) ^ dir2) / (double)(dir1 ^ dir2);
+  double scale2 = (double)((base1 - base2) ^ dir1) / (double)(dir2 ^ dir1);
 
-  return 0 <= s1 && s1 <= 1 && 0 <= s2 && s2 <= 1;
+  return 0 <= scale1 && scale1 <= 1 && 0 <= scale2 && scale2 <= 1;
 }
 
 Segment* Segment::Clone() const { return new Segment(GetA(), GetB()); }
@@ -78,22 +79,22 @@ Line::Line(const Point& start, const Point& end) {
 Line::~Line() {}
 
 bool Line::ContainsPoint(const Point& point) const {
-  Vector v = point - base_;
-  return (v ^ direction_vector_) == 0;
+  Vector vector = point - base_;
+  return (vector ^ direction_vector_) == 0;
 }
 
 bool Line::CrossesSegment(const Segment& segment) const {
-  Point a = segment.GetA();
-  Point b = segment.GetB();
-  if (ContainsPoint(a) || ContainsPoint(b)) {
+  Point point_a = segment.GetA();
+  Point point_b = segment.GetB();
+  if (ContainsPoint(point_a) || ContainsPoint(point_b)) {
     return true;
   }
 
   const Vector& start = base_;
   const Vector& end = base_ + direction_vector_;
 
-  int64_t cross_a = (end - a) ^ (start - a);
-  int64_t cross_b = (end - b) ^ (start - b);
+  int64_t cross_a = (end - point_a) ^ (start - point_a);
+  int64_t cross_b = (end - point_b) ^ (start - point_b);
 
   // if orientations are different,
   // then they have opposite signs
@@ -125,29 +126,29 @@ Ray::Ray(const Point& start, const Point& end) {
 Ray::~Ray() {}
 
 bool Ray::ContainsPoint(const Point& point) const {
-  Vector v = point - base_;
-  return (v ^ direction_vector_) == 0 && (v * direction_vector_) >= 0;
+  Vector vector = point - base_;
+  return (vector ^ direction_vector_) == 0 && (vector * direction_vector_) >= 0;
 }
 
 bool Ray::CrossesSegment(const Segment& segment) const {
-  const Vector& d1 = direction_vector_;
-  const Vector& d2 = segment.GetB() - segment.GetA();
+  const Vector& dir1 = direction_vector_;
+  const Vector& dir2 = segment.GetB() - segment.GetA();
 
-  if ((d1 ^ d2) == 0) {
+  if ((dir1 ^ dir2) == 0) {
     // segments are collinear;
     return ContainsPoint(segment.GetA()) || ContainsPoint(segment.GetB());
   }
 
-  const Vector& b1 = base_;
-  const Vector& b2 = segment.GetA() - Point(0, 0);
-  // b1 + d1 * s1 = b2 + d2 * s2;
+  const Vector& base1 = base_;
+  const Vector& base2 = segment.GetA() - Point(0, 0);
+  // base1 + dir1 * scale1 = base2 + dir2 * scale2;
   // if ray can be continued to intersect the segment,
-  // then 0 <= s1 and 0 <= s2 <= 1
+  // then 0 <= scale1 and 0 <= scale2 <= 1
 
-  double s1 = (double)((b2 - b1) ^ d2) / (double)(d1 ^ d2);
-  double s2 = (double)((b1 - b2) ^ d1) / (double)(d2 ^ d1);
+  double scale1 = (double)((base2 - base1) ^ dir2) / (double)(dir1 ^ dir2);
+  double scale2 = (double)((base1 - base2) ^ dir1) / (double)(dir2 ^ dir1);
 
-  return 0 <= s1 && 0 <= s2 && s2 <= 1;
+  return 0 <= scale1 && 0 <= scale2 && scale2 <= 1;
 }
 
 Ray* Ray::Clone() const { return new Ray(base_, base_ + direction_vector_); }
@@ -163,13 +164,23 @@ Circle::Circle(const Point& center, int64_t radius) : radius_(radius) {
 Circle::~Circle() {}
 
 static int64_t DistSquared(const Point& start, const Point& end) {
-  Vector v = end - start;
-  return v.GetX() * v.GetX() + v.GetY() * v.GetY();
+  Vector vector = end - start;
+  return vector.GetX() * vector.GetX() + vector.GetY() * vector.GetY();
 }
 
 bool Circle::ContainsPoint(const Point& point) const {
   int64_t dist_squared = DistSquared(GetCentre(), point);
   return dist_squared <= radius_ * radius_;
+}
+
+static bool IsOnCircle(const Circle& circle, const Point& point) {
+  int64_t radius = circle.GetRadius();
+  return DistSquared(circle.GetCentre(), point) == radius * radius;
+}
+
+static bool IsInsideCircle(const Circle& circle, const Point& point) {
+  int64_t radius = circle.GetRadius();
+  return DistSquared(circle.GetCentre(), point) < radius * radius;
 }
 
 // Circle crosses segment if
@@ -188,31 +199,30 @@ bool Circle::ContainsPoint(const Point& point) const {
 // then we check whether AB crosses the line
 // that is perpendicular to AB and contains the center of the circle
 bool Circle::CrossesSegment(const Segment& segment) const {
-  Point a = segment.GetA();
-  Point b = segment.GetB();
-  Point o = GetCentre();
-  int64_t oa_len = DistSquared(a, o);
-  int64_t ob_len = DistSquared(b, o);
+  Point point_a = segment.GetA();
+  Point point_b = segment.GetB();
+  Point centre = GetCentre();
   int64_t r_sqr = GetRadius() * GetRadius();
-  if (oa_len == r_sqr || ob_len == r_sqr) {
+  if (IsOnCircle(*this, point_a) || IsOnCircle(*this, point_b)) {
     return true;
   }
-  bool is_a_inside = oa_len < r_sqr;
-  bool is_b_inside = ob_len < r_sqr;
+  bool is_a_inside = IsInsideCircle(*this, point_a);
+  bool is_b_inside = IsInsideCircle(*this, point_b);
   if (is_a_inside && is_b_inside) {
     return false;
-  }
-  if (is_a_inside != is_b_inside) {
+  } else if (is_a_inside != is_b_inside) {
     return true;
   }
-  Vector oa = a - o;
-  Vector ob = b - o;
-  int64_t double_area_squared = (oa ^ ob) * (oa ^ ob);
-  int64_t ab_sqr = DistSquared(a, b);
+
+  Vector vector_a = point_a - centre;
+  Vector vector_b = point_b - centre;
+  int64_t double_area_squared = (vector_a ^ vector_b) * (vector_a ^ vector_b);
+  int64_t ab_sqr = DistSquared(point_a, point_b);
   if (double_area_squared > r_sqr * ab_sqr) {
     return false;
   }
-  Line line_ab(a, b);
+
+  Line line_ab(point_a, point_b);
   Vector ab_normal_vector(line_ab.GetA(), line_ab.GetB());
   Line normal_line(base_, base_ + ab_normal_vector);
   return normal_line.CrossesSegment(segment);
